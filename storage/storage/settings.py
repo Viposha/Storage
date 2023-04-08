6dd2009961a355a93a152888183f6b2508901ea3
Vitalii Hoshchenko <viposha@gmail.com>
2023-04-08 16:00:45 +0300

    change allowed host

--- storage/storage/settings.py
+++ storage/storage/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7b!^e3^y+@8_w7s4)45ci2u&6dyix_*90-4u^@cj9+x39*s*9!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['143.198.70.58', '127.0.0.1']
 
 
 # Application definition
